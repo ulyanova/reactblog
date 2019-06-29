31975790ea73356a79a44768dd32aa203430c1e3
ulyanova <ulyan.ov.a@yandex.ru>
2019-06-29 15:38:32 +0300

    вывод статей на страницу

--- src/app/components/ArticleList.js
+++ src/app/components/ArticleList.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Article from './Article';
-
+import articles from '../data/articlesData';
 
 export default class ArticleList extends Component {
+    constructor()
+    {
+        super(...arguments);
+
+        this.state = {
+            articles: articles
+        };
+    }
+
     render()
     {
-        console.log(this.props);
-        if(!this.props.articles.length){
+        if(!this.state.articles.length){
             return null;
         }
 
-        let articles = this.props.articles.map((article, index) => {
+        let articles = this.state.articles.map((article, index) => {
             return <Article key={index} {...article} />;
         });
 
--- src/app/pages/Main.js
+++ src/app/pages/Main.js
@@ -1,63 +1,14 @@
 import React from 'react';
 import ArticleList from '../components/ArticleList';
 
-import { connect } from 'react-redux';
-import { fetchArticles } from "../actions/articlesActions";
-
-/*@connect((store) => {
-    return {
-        articles: store.articles.articles,
-        is_fetching: store.articles.is_fetching
-    }
-})*/
-
-class Articles extends React.Component
+export default class Main extends React.Component
 {
-    constructor(props)
-    {
-        super(props);
-        let toDispatch = fetchArticles();
-        this.props.dispatch(toDispatch)
-    }
-
     render()
     {
-        console.log(this.props.children);
-        console.log(this.props);
         return (
-            <div>
-                {
-                    (!this.props.children) ?
-                        (
-                            this.props.is_fetching ?
-                                'Pending...' :
-                                (<ArticleList />)
-                        )
-                        :
-                        (this.props.children)
-                }
-            </div>
+            <>
+                <ArticleList />
+            </>
         );
     }
 }
-
-const mapStateToProps = store => {
-    console.log(store);
-    return {
-        articles: store.articles.articles,
-        is_fetching: store.articles.is_fetching
-    }
-}
-
-export default connect(mapStateToProps) (Articles)
-
-/*export default connect(state => ({
-    articles: state.articles
-}))(Articles)*/
-
-/*@connect((store) => {
-    return {
-        articles: store.articles.articles,
-        is_fetching: store.articles.is_fetching
-    }
-})*/
