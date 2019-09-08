98c8021a6b8b779e03929f678dc2a978c728b1ee
ulyanova <ulyan.ov.a@yandex.ru>
2019-09-08 14:36:07 +0300

    добавлена обработка маршрутов по типам статей для соответствующих страниц блога

--- routes/blog.js
+++ routes/blog.js
@@ -14,4 +14,44 @@ router.get('/', function (req, res, next) {
     })
 });
 
+router.get('/lifestyle', function (req, res, next) {
+    Blog.find({type: 'lifestyle'}, function (err, blog) {
+        if(err){
+            return next(err);
+        }
+
+        res.json(blog);
+    })
+});
+
+router.get('/photodiary', function (req, res, next) {
+    Blog.find({type: 'photodiary'}, function (err, blog) {
+        if(err){
+            return next(err);
+        }
+
+        res.json(blog);
+    })
+});
+
+router.get('/music', function (req, res, next) {
+    Blog.find({type: 'music'}, function (err, blog) {
+        if(err){
+            return next(err);
+        }
+
+        res.json(blog);
+    })
+});
+
+router.get('/travel', function (req, res, next) {
+    Blog.find({type: 'travel'}, function (err, blog) {
+        if(err){
+            return next(err);
+        }
+
+        res.json(blog);
+    })
+});
+
 module.exports = router;
