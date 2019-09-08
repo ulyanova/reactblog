6c236c9eddef233d275ec6275123ca0bd4296769
ulyanova <ulyan.ov.a@yandex.ru>
2019-09-08 14:33:45 +0300

    подключен store, раньше данные грузились в конструкторе; обработка статей по типу теперь идет через server.js; доработана кнопка для подгрузки статей (load more)

--- src/app/components/ArticleList.js
+++ src/app/components/ArticleList.js
@@ -1,97 +1,37 @@
 import React, {Component} from 'react';
 import Article from './Article';
-import axios from 'axios';
+
+import { connect } from 'react-redux';
+import { loadMore } from '../actions/articlesActions';
+
+@connect((store) => {
+    return {
+        articles: store.articles.articles,
+        is_fetching: store.articles.is_fetching,
+        loadButton: store.articles.loadButton
+    }
+})
 
 export default class ArticleList extends Component {
-    constructor()
-    {
-        super(...arguments);
-
-        this.state = {
-            articles: []
-        };
-
-        this._isMounted = false;
-
-        //Получение пользователей
-        axios
-            .get('http://localhost:8082/api/posts')
-            .then((response) => {
-                if(this._isMounted) {
-                    let { data } = response;
-                    this.setState({
-                        articles: data
-                    });
-                }
-            });
-    }
-
-    componentDidMount() {
-        this._isMounted = true;
-    }
-
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
-
     render()
     {
-        if(!this.state.articles.length){
+        if(!this.props.articles.length){
             return null;
         }
 
-        switch (window.location.pathname) {
-            case '/lifestyle':
-                var data = this.state.articles.filter(article => article.type == "lifestyle").slice(-5).reverse();
-                if(!data.length){
-                    return <p className="article__p center container">There are no articles of this subject</p>;
-                }
-                var articles = data.map((article, index) => {
-                    return <Article key={index} {...article} />;
-                });
-                break;
-
-            case '/photodiary':
-                var data = this.state.articles.filter(article => article.type == "photodiary").slice(-5).reverse();
-                if(!data.length){
-                    return <p className="article__p center container">There are no articles of this subject</p>;
-                }
-                var articles = data.map((article, index) => {
-                    return <Article key={index} {...article} />;
-                });
-                break;
-
-            case '/music':
-                var data = this.state.articles.filter(article => article.type == "music").slice(-5).reverse();
-                if(!data.length){
-                    return <p className="article__p center container">There are no articles of this subject</p>;
-                }
-                var articles = data.map((article, index) => {
-                    return <Article key={index} {...article} />;
-                });
-                break;
-
-            case '/travel':
-                var data = this.state.articles.filter(article => article.type == "travel").slice(-5).reverse();
-                if(!data.length){
-                    return <p className="article__p center container">There are no articles of this subject</p>;
-                }
-                var articles = data.map((article, index) => {
-                    return <Article key={index} {...article} />;
-                });
-                break;
-
-            default:
-                var data = this.state.articles.slice(-5).reverse();
-                var articles = data.map((article, index) => {
-                    return <Article key={index} {...article} />;
-                });
-        }
+        let articles = this.props.articles.map((article, index) => {
+            return <Article key={index} {...article} />;
+        });
 
         return (
-            <div className="articles container">
-                {articles}
-                <a href="#" className={(articles.length>4) ? "articles__button" + " active" : "articles__button"}>Load more</a>
+            <div className="container">
+                <div className="articles">
+                    {articles}
+                </div>
+                <a href="#" className={(!this.props.loadButton) ? "articles__button" + " active" : "articles__button"} onClick={(event) => {
+                    event.preventDefault();
+                    this.props.dispatch(loadMore(window.location.pathname));
+                }}>Load more</a>
             </div>
         );
     }
