c03de175e2c8e8f7fc99362c04568d1f7c90eec0
ulyanova <ulyan.ov.a@yandex.ru>
2019-09-17 17:17:26 +0300

    добавлены блоки: 1) вам также может понравится; 2) поделиться статьей в соц.сетях (только отрисовка, без функционала);

--- src/app/pages/ArticleFullPage.js
+++ src/app/pages/ArticleFullPage.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import ArticleFull from '../components/ArticleFull';
+import ArticlesLikeList from "../components/ArticlesLikeList";
 
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/articlesActions';
+import {fetchPost} from '../actions/articlesActions';
+
 
 @connect((store) => {
     return {
         articles: store.articles.articles,
         is_fetching: store.articles.is_fetching,
-        loadButton: store.articles.loadButton
     }
 })
 
@@ -38,11 +39,27 @@ export default class ArticleFullPage extends Component {
         });
 
         return (
-            <div className="container">
-                <article className="article">
-                    {article}
-                </article>
-            </div>
+            <>
+                <div className="container clearfix">
+                    <article className="article">
+                        {article}
+                    </article>
+                    <div className="share">
+                        <p className="share__p">share</p>
+                        <a href="#" className="share__a"><i className="icon-facebook"></i></a>
+                        <a href="#" className="share__a"><i className="icon-twitter"></i></a>
+                        <a href="#" className="share__a"><i className="icon-gplus"></i></a>
+                        <a href="#" className="share__a"><i className="icon-tumblr"></i></a>
+                        <a href="#" className="share__a"><i className="icon-pinterest-circled"></i></a>
+                    </div>
+                </div>
+                <div className="you-may-also-like">
+                    <div className="container">
+                        <p className="article-type__p">you may also like</p>
+                        <ArticlesLikeList />
+                    </div>
+                </div>
+            </>
         );
     }
 }
