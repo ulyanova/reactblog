ae249a0449b87839ff871b27f1d5b1a17f52d033
ulyanova <ulyan.ov.a@yandex.ru>
2019-09-17 17:15:51 +0300

    добавлена обработка массивов данных (текст, картинки) при отображении полного варианта статьи

--- src/app/components/ArticleFull.js
+++ src/app/components/ArticleFull.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom';
 
 export default class ArticleFull extends Component {
     render() {
-        return (
-            <>
-                <img src={this.props.img[0]} className="article__img"/>
-                <h5 className="article-type__p">{this.props.type}</h5>
-                <h3 className="article__h">{this.props.title}</h3>
-                <p className="article__p">{this.props.body[0]}</p>
-                <p className="article__p">{this.props.body[1]}</p>
-                <img src={this.props.img[1]} className="article__img"/>
-                <img src={this.props.img[2]} className="article__img"/>
-                <img src={this.props.img[3]} className="article__img"/>
-                <p className="article__p">{this.props.body[2]}</p>
-                <p className="article__p">{this.props.body[3]}</p>
-            </>
-        );
+        return <>
+            <img src={this.props.img + '.jpg'} className="article__img"/>
+            <h5 className="article-type__p">{this.props.type}</h5>
+            <h3 className="article__h">{this.props.title}</h3>
+            { this.props.body.map((item) => (
+                <p key={item} className="article__p">{item}</p>
+            ))}
+
+            { this.props.images.map((image) => (
+                <img key={image} src={image} className="article__img"/>
+            ))}
+
+            { this.props.bodyItalic.map((item) => (
+                <p key={item} className="article__p article__p_italic">{item}</p>
+            ))}
+
+            { this.props.bodyFull.map((item) => (
+                <p key={item} className="article__p">{item}</p>
+            ))}
+        </>
     }
 }
