58bb2946d173a7d7a3214aeed6683ddffbb22749
ulyanova <ulyan.ov.a@yandex.ru>
2019-09-17 17:06:46 +0300

    в компоненте отрисовки статьи добавлено условие для показа картинки в полном разрешении или миниатюры

--- src/app/components/Article.js
+++ src/app/components/Article.js
@@ -5,7 +5,7 @@ export default class Article extends Component {
     render() {
         return (
             <article className="article">
-                <img src={this.props.img[0]} className="article__img"/>
+                <img src={(this._reactInternalFiber.index == 0) ? this.props.img + '.jpg' : this.props.img + 'min.jpg'} className="article__img"/>
                 <h5 className="article-type__p">{this.props.type}</h5>
                 <p><Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link></p>
                 <p className="article__p">{this.props.body[0]}</p>
