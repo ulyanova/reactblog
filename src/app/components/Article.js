8674a50e7502252d7610b806b18b4621c9007c39
ulyanova <ulyan.ov.a@yandex.ru>
2019-09-28 17:32:28 +0300

    Картинка статьи на главной странице теперь тоже ссылка

--- src/app/components/Article.js
+++ src/app/components/Article.js
@@ -5,7 +5,7 @@ export default class Article extends Component {
     render() {
         return (
             <article className="article">
-                <img src={(this._reactInternalFiber.index == 0) ? this.props.img + '.jpg' : this.props.img + 'min.jpg'} className="article__img"/>
+                <Link to={'/post/' + this.props.id} className="img__link"><img src={(this._reactInternalFiber.index == 0) ? this.props.img + '.jpg' : this.props.img + 'min.jpg'} className="article__img"/></Link>
                 <h5 className="article-type__p">{this.props.type}</h5>
                 <p><Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link></p>
                 <p className="article__p">{this.props.body[0]}</p>
