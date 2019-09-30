14be18069da36320607451a71330694d64b22587
ulyanova <ulyan.ov.a@yandex.ru>
2019-09-30 10:28:08 +0300

    1) картинки статей теперь тоже ссылки 2) эффекты наведения

--- src/app/components/Article.js
+++ src/app/components/Article.js
@@ -9,7 +9,7 @@ export default class Article extends Component {
                 <h5 className="article-type__p">{this.props.type}</h5>
                 <p><Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link></p>
                 <p className="article__p">{this.props.body[0]}</p>
-                <a href="#" className="article__a">leave a comment</a>
+                <Link to={'/post/' + this.props.id} className="article__a">show more</Link>
             </article>
         );
     }
--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -10,7 +10,7 @@ export default class Footer extends Component {
                         <a href="#" className="footer__a">Privacy</a>
                     </div>
                     <div className="footer-social">
-                        <p className="footer__a">Follow</p>
+                        <p className="footer__p">Follow</p>
                         <a href="#" className="social__a"><i className="icon-facebook"></i></a>
                         <a href="#" className="social__a"><i className="icon-twitter"></i></a>
                         <a href="#" className="social__a"><i className="icon-instagram-1"></i></a>
