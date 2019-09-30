10bf0334e6efec57e42bcbbc6aef054b89b9676f
ulyanova <ulyan.ov.a@yandex.ru>
2019-09-30 16:27:35 +0300

    картинки в блоке you may also like тоже ссылки на статью

--- src/app/components/ArticlesLike.js
+++ src/app/components/ArticlesLike.js
@@ -5,7 +5,7 @@ export default class ArticlesLike extends Component {
     render() {
         return (
             <figure className="articles-preview__figure">
-                <img src={this.props.img + 'min.jpg'} className="article__img"/>
+                <Link to={'/post/' + this.props.id} className="img__link"><img src={this.props.img + 'min.jpg'} className="article__img"/></Link>
                 <p><Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link></p>
             </figure>
         );
