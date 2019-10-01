aa4f82c1fe39aa20cebb12f2a305f99d8bbdcc89
ulyanova <ulyan.ov.a@yandex.ru>
2019-10-01 14:27:14 +0300

    отключены ссылки, т.к. контента нет

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -6,14 +6,24 @@ export default class Footer extends Component {
             <div className="footer">
                 <div className="container footer__div">
                     <div className="footer-links">
-                        <a href="#" className="footer__a">Terms and conditions</a>
-                        <a href="#" className="footer__a">Privacy</a>
+                        <a href="#" className="footer__a" onClick={(event) => {
+                            event.preventDefault();
+                        }}>Terms and conditions</a>
+                        <a href="#" className="footer__a" onClick={(event) => {
+                            event.preventDefault();
+                        }}>Privacy</a>
                     </div>
                     <div className="footer-social">
                         <p className="footer__p">Follow</p>
-                        <a href="#" className="social__a"><i className="icon-facebook"></i></a>
-                        <a href="#" className="social__a"><i className="icon-twitter"></i></a>
-                        <a href="#" className="social__a"><i className="icon-instagram-1"></i></a>
+                        <a href="#" className="social__a" onClick={(event) => {
+                            event.preventDefault();
+                        }}><i className="icon-facebook"></i></a>
+                        <a href="#" className="social__a" onClick={(event) => {
+                            event.preventDefault();
+                        }}><i className="icon-twitter"></i></a>
+                        <a href="#" className="social__a" onClick={(event) => {
+                            event.preventDefault();
+                        }}><i className="icon-instagram-1"></i></a>
                     </div>
                 </div>
             </div>
