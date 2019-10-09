5dcf9609a52fadbd47722e193f00135aa5a544cd
ulyanova <ulyan.ov.a@yandex.ru>
2019-10-09 11:06:53 +0300

    При нажатии на кнопку Load more на главной странице данные загружались повторно и компонент перерисовывался полностью, в результате откидывало вверх страницы.
    Теперь компонент упрощен и переработан: в редюссере загружаются все статьи, а кол-во отображаемых статей изменяется в state компонента ArticlesList. Компонент не перерисовывается, изменяется только кол-во отображаемых статей. Проблемы с откидываем в вверх компонента в результате тоже нет.

--- src/app/actions/articlesActions.js
+++ src/app/actions/articlesActions.js
@@ -7,13 +7,6 @@ export function fetchArticles(type) {
     }
 }
 
-export function loadMore(type) {
-    return {
-        type: 'LOAD_MORE',
-        payload: axios.get('http://localhost:8082/api/posts' + type)
-    }
-}
-
 export function fetchPost(id) {
     return {
         type: 'FETCH_POST',
