6ae22c2d06b7714d929179b69069659a844f0ec9
ulyanova <ulyan.ov.a@yandex.ru>
2019-10-14 13:24:40 +0300

    теперь просто выводит список, переданных через props, комментариев

--- src/app/components/CommentsList.js
+++ src/app/components/CommentsList.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
 import Comment from './Comment';
-import AuthModal from './AuthModal';
 
 import { connect } from 'react-redux';
-import NewComment from "./NewComment";
 
 @connect((store) => {
     return {
         comments: store.comments.comments,
-        is_comments_fetching: store.comments.is_fetching,
         users: store.users.users,
-        is_users_fetching: store.users.is_fetching,
         authorization: store.users.authorization
     }
 })
@@ -18,35 +14,23 @@ import NewComment from "./NewComment";
 export default class CommentsList extends Component {
     render()
     {
-        let comments = this.props.comments.map((comment, index) => {
+        let comments = this.props.replies.map((comment, index) => {
             let user = this.props.users.find(user => user.id === comment.userId);
             return <Comment key={index} user={user} {...comment} />;
         });
 
         return (
-            <div className="container">
-                <div className="comments">
-                    {
-                        (this.props.comments.length)
-                            ?
-                            <p className="article-type__p">{this.props.comments.length + ' comments'}</p>
-                            :
-                            <p className="article-type__p">Be the first to comment on this post!</p>
-                    }
-                    {
-                        (this.props.is_comments_fetching || this.props.is_users_fetching)
-                            ?
-                            'Pending...'
-                            :
-                            (comments)
-                    }
-                    <div className="comment comment-new">
-                        {
-                            (!this.props.authorization) ? <AuthModal /> : <NewComment />
-                        }
-                    </div>
-                </div>
-            </div>
+            <>
+                {comments}
+            </>
         );
     }
 }
+
+/*{
+                    (this.props.comments.length)
+                        ?
+                        <p className="article-type__p">{this.props.comments.length + ' comments'}</p>
+                        :
+                        <p className="article-type__p">Be the first to comment on this post!</p>
+                }*/
