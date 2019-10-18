fe489f4f46a06891ab62959d847e6d649b351624
ulyanova <ulyan.ov.a@yandex.ru>
2019-10-18 17:22:13 +0300

    Изменено поле ввода комментариев: ранее использовался textarea (высота рассчитывалась в компоненте Divsize (копия div), и высота присваивалась textarea, во время тестирования с вложенными комментариями работало некорректно); теперь используется div role="textbox"

--- src/app/components/NewComment.js
+++ src/app/components/NewComment.js
@@ -1,51 +1,16 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import {addComment} from "../actions/commentsActions";
-import DivSize from "./DivSize";
+import {addComment, updateCount} from "../actions/commentsActions";
 
 @connect((store) => {
     return {
         auth_user: store.users.auth_user,
-        all_comments: store.comments.all_comments,
         articles: store.articles.articles,
-        textHeight: store.comments.textHeight
+        count: store.comments.count
     }
 })
 
 export default class NewComment extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            usermessage: '',
-            count: 0
-        };
-
-        this.handleChangeMessage = this.handleChangeMessage.bind(this);
-    }
-
-    handleChangeMessage(event) {
-        this.setState({usermessage: event.target.value});
-    }
-
-    componentDidMount() {
-        //массив всех комментариев
-        let comments = this.props.all_comments;
-        //массив с массивами вложенных комментариев
-        let replies = this.props.all_comments.map(function (comment) {
-            return comment.replies;
-        });
-        //объединяем все массивы вложенных комментариев в один массив
-        replies = replies.reduce((prev, item) => prev.concat(item), Array(0));
-        //объединяем все комментарии в один массив
-        comments = comments.concat(replies);
-        //выбираем только id комментариев
-        let commentsId = comments.map((comment) => comment.id);
-        //выбираем максимальное значение id
-        let max = commentsId.reduce((prev, cur) => cur > prev ? cur : prev);
-        this.setState({count: Number(max)});
-    }
-
     render() {
         return (
             <>
@@ -54,24 +19,26 @@ export default class NewComment extends Component {
                         <p className="comment-userimg comment-userimg__p">{this.props.auth_user.name.match(/\b\w/g).join('')}</p>
                 }
                 <div id="text" className="comment-block">
-                    <DivSize usermessage={this.state.usermessage} />
-                    <form>
-                        <textarea id="usermessage" className="comment__textarea" style={{height: this.props.textHeight}} placeholder="JOIN THE DISCUSSION" value={this.state.usermessage} onChange={this.handleChangeMessage}></textarea>
-                    </form>
+                    <div id="usermessage" className="comment__textarea" tabIndex="0" contentEditable="true" suppressContentEditableWarning={true}
+                         role="textbox" aria-multiline="true" data-placeholder="JOIN THE DISCUSSION...">
+                    </div>
                     <a href="#" className="comment__a" onClick={(event) => {
                         event.preventDefault();
+                        let usermessage = document.getElementById('usermessage').textContent;
                         let comment = {
                             postId: this.props.articles[0].id,
-                            id: String(this.state.count + 1),
+                            id: String(Number(this.props.count) + 1),
                             userId: this.props.auth_user.id,
-                            body: this.state.usermessage
+                            body: usermessage,
+                            replies: []
                         }
                         this.props.dispatch(addComment(comment));
-                        this.setState({usermessage: '', count: this.state.count + 1});
+                        this.props.dispatch(updateCount(Number(this.props.count) + 1));
+                        document.getElementById('usermessage').textContent = '';
                     }}>send</a>
                     <a href="#" className="comment__a" onClick={(event) => {
                         event.preventDefault();
-                        this.setState({usermessage: ''});
+                        document.getElementById('usermessage').textContent = '';
                     }}>cancel</a>
                 </div>
             </>
--- src/app/components/NewReply.js
+++ src/app/components/NewReply.js
@@ -1,51 +1,16 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import {addReply} from "../actions/commentsActions";
-import DivSize from "./DivSize";
+import {addReply, updateCount} from "../actions/commentsActions";
 
 @connect((store) => {
     return {
         auth_user: store.users.auth_user,
-        all_comments: store.comments.all_comments,
         articles: store.articles.articles,
-        textHeight: store.comments.textHeight
+        count: store.comments.count
     }
 })
 
 export default class NewReply extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            usermessage: '',
-            count: 0
-        };
-
-        this.handleChangeMessage = this.handleChangeMessage.bind(this);
-    }
-
-    handleChangeMessage(event) {
-        this.setState({usermessage: event.target.value});
-    }
-
-    componentDidMount() {
-        //массив всех комментариев
-        let comments = this.props.all_comments;
-        //массив с массивами вложенных комментариев
-        let replies = this.props.all_comments.map(function (comment) {
-            return comment.replies;
-        });
-        //объединяем все массивы вложенных комментариев в один массив
-        replies = replies.reduce((prev, item) => prev.concat(item), Array(0));
-        //объединяем все комментарии в один массив
-        comments = comments.concat(replies);
-        //выбираем только id комментариев
-        let commentsId = comments.map((comment) => comment.id);
-        //выбираем максимальное значение id
-        let max = commentsId.reduce((prev, cur) => cur > prev ? cur : prev);
-        this.setState({count: Number(max)});
-    }
-
     render() {
         return (
             <div className="comment comment-new">
@@ -54,26 +19,28 @@ export default class NewReply extends Component {
                         <p className="comment-userimg comment-userimg__p">{this.props.auth_user.name.match(/\b\w/g).join('')}</p>
                 }
                 <div id="text" className="comment-block">
-                    <DivSize usermessage={this.state.usermessage} />
-                    <form>
-                        <textarea id="usermessage" className="comment__textarea" style={{height: this.props.textHeight}} placeholder="JOIN THE DISCUSSION" value={this.state.usermessage} onChange={this.handleChangeMessage}></textarea>
-                    </form>
+                    <div id="usermessage" className="comment__textarea" tabIndex="0" contentEditable="true" suppressContentEditableWarning={true}
+                         role="textbox" aria-multiline="true">
+                    </div>
                     <a href="#" className="comment__a" onClick={(event) => {
                         event.preventDefault();
+                        let usermessage = document.getElementById('usermessage').textContent;
                         let comment = {
                             postId: this.props.articles[0].id,
-                            id: String(this.state.count + 1),
+                            id: String(Number(this.props.count) + 1),
                             commentId: this.props.commentId,
                             userId: this.props.auth_user.id,
-                            body: this.state.usermessage
+                            body: usermessage,
+                            replies: []
                         }
                         this.props.dispatch(addReply(comment));
-                        this.setState({usermessage: '', count: this.state.count + 1});
+                        this.props.dispatch(updateCount(Number(this.props.count) + 1));
+                        document.getElementById('usermessage').textContent = '';
                         this.props.show(false);
                     }}>send</a>
                     <a href="#" className="comment__a" onClick={(event) => {
                         event.preventDefault();
-                        this.setState({usermessage: ''});
+                        document.getElementById('usermessage').textContent = '';
                         this.props.show(false);
                     }}>cancel</a>
                 </div>
