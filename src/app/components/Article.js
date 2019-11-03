339c337c65c77aa7e7ec5e3f4956884d8535b9e0
ulyanova <ulyan.ov.a@yandex.ru>
2019-11-03 10:46:33 +0300

    добавлены или изменены названия классов, удален clearfix, если использовался

--- src/app/components/Article.js
+++ src/app/components/Article.js
@@ -8,7 +8,7 @@ export default class Article extends Component {
                 <Link to={'/post/' + this.props.id} className="img__link"><img src={(this._reactInternalFiber.index == 0) ?
                     this.props.img + '.jpg' : this.props.img + 'min.jpg'} className="article__img"/></Link>
                 <h5 className="article-type__p">{this.props.type}</h5>
-                <p><Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link></p>
+                <Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link>
                 <p className="article__p">{this.props.body[0]}</p>
                 <Link to={'/post/' + this.props.id} className="article__a">show more</Link>
             </article>
