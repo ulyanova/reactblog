c5093a74eb9a1a6ba4949cadf9e1a2a1ea8a424d
ulyanova <ulyan.ov.a@yandex.ru>
2019-11-03 12:46:05 +0300

    тэг р удален

--- src/app/components/ArticlesLike.js
+++ src/app/components/ArticlesLike.js
@@ -7,7 +7,7 @@ export default class ArticlesLike extends Component {
             <figure className="articles-preview__figure">
                 <Link to={'/post/' + this.props.id} className="img__link"><img src={this.props.img + 'min.jpg'}
                                                                                className="article__img"/></Link>
-                <p><Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link></p>
+                <Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link>
             </figure>
         );
     }
