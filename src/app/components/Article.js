e487b0d0040afb703f51f105d1c3892594ee675d
ulyanova <ulyan.ov.a@yandex.ru>
2019-11-09 15:00:34 +0300

    1) картинки  теперь сделаны через background, для адаптивности 2) переработаны стили для картинок 3) доработаны отступы для адаптивности

--- src/app/components/Article.js
+++ src/app/components/Article.js
@@ -5,8 +5,7 @@ export default class Article extends Component {
     render() {
         return (
             <article className="article">
-                <Link to={'/post/' + this.props.id} className="img__link"><img src={(this._reactInternalFiber.index == 0) ?
-                    this.props.img + '.jpg' : this.props.img + 'min.jpg'} className="article__img"/></Link>
+                <Link to={'/post/' + this.props.id} className="article-img" style={{backgroundImage: 'url('+ this.props.img +')'}}></Link>
                 <h5 className="article-type__p">{this.props.type}</h5>
                 <Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link>
                 <p className="article__p">{this.props.body[0]}</p>
--- src/app/components/ArticlesLike.js
+++ src/app/components/ArticlesLike.js
@@ -5,8 +5,7 @@ export default class ArticlesLike extends Component {
     render() {
         return (
             <figure className="articles-preview__figure">
-                <Link to={'/post/' + this.props.id} className="img__link"><img src={this.props.img + 'min.jpg'}
-                                                                               className="article__img"/></Link>
+                <Link to={'/post/' + this.props.id} className="article-img" style={{backgroundImage: 'url('+ this.props.img +')'}}></Link>
                 <Link to={'/post/' + this.props.id} className="article__h">{this.props.title}</Link>
             </figure>
         );
